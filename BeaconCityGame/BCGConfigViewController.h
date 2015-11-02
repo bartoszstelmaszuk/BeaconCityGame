2e636a4ced4fb64ffe403c07624ee80f8959d6a8
bartoszstelmaszuk <stelmaszuk.b@gmail.com>
2015-11-02 18:37:25 +0100

    BCG: added tracking and config view controllers

---
+++ BeaconCityGame/BCGConfigViewController.h
@@ -0,0 +1,13 @@
+//
+//  BCGConfigViewController.h
+//  BeaconCityGame
+//
+//  Created by Bartosz Stelmaszuk on 02/11/15.
+//  Copyright © 2015 Bartosz Stelmaszuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BCGConfigViewController : UIViewController
+
+@end
---
+++ BeaconCityGame/BCGTrackingViewController.h
@@ -0,0 +1,13 @@
+//
+//  BCGTrackingViewController.h
+//  BeaconCityGame
+//
+//  Created by Bartosz Stelmaszuk on 02/11/15.
+//  Copyright © 2015 Bartosz Stelmaszuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BCGTrackingViewController : UIViewController
+
+@end
