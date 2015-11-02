5d568c9837da52247d5d29e8e8e77f86d3aa0d85
bartoszstelmaszuk <stelmaszuk.b@gmail.com>
2015-11-02 17:57:27 +0100

    Initial Commit

---
+++ BeaconCityGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BeaconCityGame
+//
+//  Created by Bartosz Stelmaszuk on 02/11/15.
+//  Copyright © 2015 Bartosz Stelmaszuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BeaconCityGame/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BeaconCityGame
+//
+//  Created by Bartosz Stelmaszuk on 02/11/15.
+//  Copyright © 2015 Bartosz Stelmaszuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
